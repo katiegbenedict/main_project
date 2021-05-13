ab04b57cb250bb9979d1741ea05d6506ef312830
katiegbenedict <80117199+katiegbenedict@users.noreply.github.com>
2021-05-13 12:14:58 -0500

    Add files via upload

---
+++ main_project/main_project_js/main_project.js
@@ -0,0 +1,87 @@
+$(document).ready(function() {
+
+    $(".number_change_00_01").mouseenter(function() {
+        $(".number_change_00_01").html("1");
+    });
+
+    $(".number_change_01_02").mouseenter(function() {
+        $(".number_change_01_02").html("2");
+    });
+
+    $(".number_change_02_03").mouseenter(function() {
+        $(".number_change_02_03").html("3");
+    });
+
+    $(".number_change_03_04").mouseenter(function() {
+        $(".number_change_03_04").html("4");
+    });
+
+    $(".number_change_04_05").mouseenter(function() {
+        $(".number_change_04_05").html("5");
+    });
+
+    $(".number_change_05_06").mouseenter(function() {
+        $(".number_change_05_06").html("6");
+    });
+
+    $(".number_change_06_07").mouseenter(function() {
+        $(".number_change_06_07").html("7");
+    });
+
+    $(".number_change_07_08").mouseenter(function() {
+        $(".number_change_07_08").html("8");
+    });
+
+    $(".number_change_08_09").mouseenter(function() {
+        $(".number_change_08_09").html("9");
+    });
+
+    $(".number_change_09_10").mouseenter(function() {
+        $(".number_change_09_10").html("10");
+    });
+
+    $(".number_change_10_11").mouseenter(function() {
+        $(".number_change_10_11").html("1");
+    });
+
+    $(".number_change_11_12").mouseenter(function() {
+        $(".number_change_11_12").html("2");
+    });
+
+    $(".number_change_12_13").mouseenter(function() {
+        $(".number_change_12_13").html("3");
+    });
+
+    $(".number_change_13_14").mouseenter(function() {
+        $(".number_change_13_14").html("4");
+    });
+
+    $(".number_change_14_15").mouseenter(function() {
+        $(".number_change_14_15").html("5");
+    });
+
+    $(".number_change_15_16").mouseenter(function() {
+        $(".number_change_15_16").html("6");
+    });
+
+    $(".number_change_16_17").mouseenter(function() {
+        $(".number_change_16_17").html("7");
+    });
+
+    $(".number_change_17_18").mouseenter(function() {
+        $(".number_change_17_18").html("8");
+    });
+
+    $(".number_change_18_19").mouseenter(function() {
+        $(".number_change_18_19").html("9");
+    });
+
+    $(".number_change_19_20").mouseenter(function() {
+        $(".number_change_19_20").html("20");
+    });
+
+    $(".number_change_20_21").mouseenter(function() {
+        $(".number_change_20_21").html("1");
+    });
+});
+
